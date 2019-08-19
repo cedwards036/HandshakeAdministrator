3b35e07494c94a0b4d744096dd6c09cbe6971041
Christopher Edwards <cedwards036@gmail.com>
2019-08-19 09:18:18 -0400

    Update daily verification to be compatible with VerificationReport class

--- rule_sets/daily_verification.py
+++ rule_sets/daily_verification.py
@@ -7,16 +7,16 @@ from src.event_rules import (
     events_are_invite_only_iff_not_university_wide
 )
 from src.utils import print_and_write_to_file
-from src.verification_report import run_rule_verifications
+from src.verification_report import run_rule_verifications, VerificationReport
 
 EVENTS_INSIGHTS_LINK = 'https://app.joinhandshake.com/analytics/explore_embed?insights_page=ZXhwbG9yZS9nZW5lcmF0ZWRfaGFuZHNoYWtlX3Byb2R1Y3Rpb24vZXZlbnRzP3FpZD02aEZwc2JlQ2wzMVROUkpZTU5Xa043JmVtYmVkX2RvbWFpbj1odHRwczolMkYlMkZhcHAuam9pbmhhbmRzaGFrZS5jb20mdG9nZ2xlPWZpbA=='
 APPTS_INSIGHTS_LINK = 'https://app.joinhandshake.com/analytics/explore_embed?insights_page=ZXhwbG9yZS9nZW5lcmF0ZWRfaGFuZHNoYWtlX3Byb2R1Y3Rpb24vYXBwb2ludG1lbnRzP3FpZD1UbFFzVVVtRWpTRUNQNldBMVQzTk1zJmVtYmVkX2RvbWFpbj1odHRwczolMkYlMkZhcHAuam9pbmhhbmRzaGFrZS5jb20mdG9nZ2xlPWZpbA=='
 
-REPORT_FILEPATH = 'C:\\Users\\cedwar42\\Downloads\\event_rule_verification_results.txt'
+REPORT_FILEPATH = 'C:\\Users\\cedwar42\\Downloads\\daily_rule_verification_results.txt'
 
 
-def handle_event_rule_report(report_text: str):
-    print_and_write_to_file(report_text, REPORT_FILEPATH)
+def handle_daily_rule_report(report: VerificationReport):
+    print_and_write_to_file(str(report), REPORT_FILEPATH)
 
 
 with HandshakeSession(HANDSHAKE_URL, HANDSHAKE_EMAIL) as browser:
@@ -31,4 +31,4 @@ if __name__ == '__main__':
         (jhu_owned_events_are_prefixed_correctly, events),
         (events_are_invite_only_iff_not_university_wide, events),
         (past_appointments_have_finalized_status, appts)
-    ], handle_event_rule_report)
+    ], handle_daily_rule_report)
